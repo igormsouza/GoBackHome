0ee4ebb1f034e0cc3f1ef1764b6b27b81c68975d
igor moreira <Igor.Moreira@techdata.com>
2018-06-08 10:25:50 +0100

    add web api

---
+++ BusGoBackHome/BusGobackHome.WebCore/Controllers/ApiHomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BusGobackHome.WebCore.Models;
+
+namespace BusGobackHome.WebCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class ApiHomeController : Controller
+    {
+        [HttpGet("")]
+        public RealTime Index()
+        {
+            var result = new RealTime();
+
+            try
+            {
+                var item = new TimeList();
+                item.Refresh();
+                result.TimeList = item;
+                result.Succed = true;
+            }
+            catch (Exception ex)
+            {
+                result.ErrorMsg = ex.Message + " - " + ex.InnerException;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ BusGoBackHome/BusGobackHome.WebCore/Models/RealTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusGobackHome.WebCore.Models
+{
+    public class RealTime
+    {
+        public TimeList TimeList { get; set; }
+
+        public bool Succed { get; set; }
+
+        public string ErrorMsg { get; set; }
+    }
+}
