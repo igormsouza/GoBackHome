30bd347b8732c89b03006b6664217808508a72d2
igor moreira <igor_ba05@hotmail.com>
2018-09-20 12:01:20 +0100

    Integration external iframe created

---
+++ BusGoBackHome/BusGobackHome.WebCore/Controllers/Integration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BusGobackHome.WebCore.Models;
+
+namespace BusGobackHome.WebCore.Controllers
+{
+    public class IntegrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            var item = new TimeList();
+            item.Refresh();
+            return View(item);
+        }
+    }
+}
